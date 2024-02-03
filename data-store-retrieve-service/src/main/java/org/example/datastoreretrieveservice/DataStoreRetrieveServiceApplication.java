893ca0d8434f37183895c46442311fc7b2407ff0
ashish2812 <ashish.k.singh1209@gmail.com>
2024-02-03 21:27:19 +0530

    added other services

---
+++ data-store-retrieve-service/src/main/java/org/example/datastoreretrieveservice/DataStoreRetrieveServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.datastoreretrieveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStoreRetrieveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStoreRetrieveServiceApplication.class, args);
+    }
+
+}
