dab3724a3f5ef2eb86215fe3ebffeba17209f823
ashish2812 <ashish.k.singh1209@gmail.com>
2024-02-03 20:44:48 +0530

    new commit

---
+++ DataServiceRetrieveService/src/main/java/org/example/dataserviceretrieveservice/DataServiceRetrieveServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.dataserviceretrieveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataServiceRetrieveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataServiceRetrieveServiceApplication.class, args);
+    }
+
+}
---
+++ DataServiceRetrieveService/src/test/java/org/example/dataserviceretrieveservice/DataServiceRetrieveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dataserviceretrieveservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServiceRetrieveServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
